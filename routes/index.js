c1cfc143b816dffa11e2e49e37368d60a3a389d4
Camila Inácio Belo da Silva <camilaibs@gmail.com>
2015-03-10 01:00:54 -0300

    angular com express

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,19 @@
 //function que renderiza a index
 exports.index = function(req, res){
 	res.render('index');
-}
+};
+
+exports.lista = function(req, res){
+    res.json({
+        titulo: 'Guangues de Nova Iorque',
+        diretor: 'Martin Scorsese',
+        ano: 2002
+    });
+};
+
+
+exports.grava = function(req, res){
+    var filme = req.body;
+    console.log(filme);
+    res.send('Filme ' + filme.titulo + ' recebido no servidor');
+};
